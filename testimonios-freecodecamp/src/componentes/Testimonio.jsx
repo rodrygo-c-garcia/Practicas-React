d6e47a5176e448497b0c358f64fedcbf46d6e545
Rodrigo Colque Garcia <colqueariel023@gmail.com>
2022-11-15 23:16:03 -0400

    Se concluyo mi practica 1

--- testimonios-freecodecamp/src/componentes/Testimonio.jsx
+++ testimonios-freecodecamp/src/componentes/Testimonio.jsx
@@ -7,26 +7,19 @@ function Testimonio(props) {
     <div className="contenedor-testimonio">
       <img
         className="img-testimonio"
-        src="https://i.ibb.co/xC8SRrY/Shawn-bf42c7b909dae3111d18a21870ee6ca5.png"
+        src={props.imagen}
         alt="imagen 2"
         border="0"
       />
       <Card className="contenedor-texto-testimonio" style={{ width: "100%" }}>
         <Card.Body className="contenedor-body">
           <Card.Title className="nombre-testimonio">
-            Shawn Wang en Singapur
+            <strong>{props.nombre}</strong> en <strong>{props.pais}</strong>
           </Card.Title>
           <Card.Subtitle className="mb-2 text-muted cargo-testimonio">
-            Ingeniero de Software en Amazon
+            <strong>{props.cargo}</strong> en <strong>{props.empresa}</strong>
           </Card.Subtitle>
-          <Card.Text>
-            &quot;Da miedo cambiar de carrera. Solo gané la confianza de que
-            podía programar trabajando a través de los cientos de horas de
-            lecciones gratuitas en freeCodeCamp. Dentro de un año tuve un
-            trabajo de seis cifras como ingeniero de software.{" "}
-            <b>freeCodeCamp cambió mi vida.&quot;</b>{" "}
-          </Card.Text>
-          <Card.Link href="#">Card Link</Card.Link>
+          <Card.Text>{props.testimonio}</Card.Text>
         </Card.Body>
       </Card>
     </div>
